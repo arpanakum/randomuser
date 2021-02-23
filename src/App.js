c6d35c8e91bff6f164fb26374f66de6d5351d267
arpanakum <arpana.kum@gmail.com>
2021-02-23 11:42:40 +0530

    addingfiles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useState,useEffect,useMemo} from 'react';
 import './App.css';
+import User from './User'
 
 function App() {
+
+  const [data,setData] = useState([]);
+  const [count,setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => setData(users));
+  }, [])
+
+ const memoUser = useMemo(() => {
+    return <User userData={data}></User>
+  },[data])
+
+  const handleIncrement = () => {
+    setCount(prevCount => prevCount+1);
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Counter: {count}</h1>
+      <button onClick={handleIncrement}>+</button>
+      <h2>Random Users</h2>
+      {memoUser}
     </div>
   );
 }
